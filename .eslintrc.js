23ecc58a7197e3dd99a0a926c10ece6355214b95
Gerald Amezcua <gzamezcua1@gmail.com>
2020-09-02 10:25:44 -0500

    [ESLINT]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,8 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
     "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "airbnb-base"
     ],
-    "parser": "typescript-eslint-parser",
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 12,
         "sourceType": "module"
